68fc2c3627ed90011a22511c3844a4b6e5ab78d4
brenda nyokabi <brndnyokabi@gmail.com>
2017-03-06 18:14:33 +0300

    the solution test program

---
+++ test_solutions.py
@@ -0,0 +1,9 @@
+import unitest 
+from solutionOne import solutions
+
+class primeTestCase(unittest.TestCase):
+    """ sample Testing .py`."""
+    def  TestSolution(initest.TestCase):
+    	def test_solution(self):
+    		self.AssertTrue(solution(100,200,"+")300)
+    		
